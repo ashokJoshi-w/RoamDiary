878f4137cb2d3b582551a8e372be33f3ee9e3da4
ashok <ashokjoshi2451@gmail.com>
2026-05-20 13:40:20 +0530

    Database issure resovle, multter and uttilites

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profileImageUrl: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
