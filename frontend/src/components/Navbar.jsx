349fb09590915e668b69b259b0768c5900a785ea
ashok <ashokjoshi2451@gmail.com>
2026-05-28 22:41:56 +0530

    cards error fixed and navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProfileInfo from './Cards/ProfileInfo';
+import SearchBar from './Input/SearchBar';
+
+const Navbar = React.memo(({ userInfo, searchQuery, setSearchQuery, onSearchNote, handleClearSearch }) => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
+  const handleSearch = () => {
+    if (searchQuery) {
+      onSearchNote(searchQuery);
+    }
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery('');
+    handleClearSearch();
+  };
+
+  return (
+    <div className="bg-white flex flex-col md:flex-row items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10 transition-all">
+      <h2 className="text-2xl font-bold text-purple-600 tracking-tight py-2">
+        Trip <span className="text-purple-400">Scribe</span>
+      </h2>
+
+      {userInfo && (
+        <div className="flex-1 flex items-center justify-between md:justify-end gap-3 w-full md:w-auto mt-2 md:mt-0">
+          <SearchBar
+            value={searchQuery}
+            onChange={({ target }) => setSearchQuery(target.value)}
+            handleSearch={handleSearch}
+            onClearSearch={onClearSearch}
+          />
+
+          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+        </div>
+      )}
+    </div>
+  );
+});
+
+export default Navbar;
